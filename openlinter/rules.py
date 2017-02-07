29c3c6ed10b2f404ba0031f9614ab0f4eb99da50
Frances Hocutt <frances.hocutt+gh@gmail.com>
2017-02-06 23:29:43 -0800

    [#21] Add the ability to check for file presence in a local repo
    * Add ruleset-parsing and file-checking functionality to openlinter.py
    * Add functions to check for file presence and content in rules.py
    * Move to a dict-type structure in the "files_exist" ruleset in rules.yml
    * Draft most of the rest of the config flags for functionality to be added
    
    Note TODOs: this works, but is not very testable and should be refactored.

--- openlinter/rules.py
+++ openlinter/rules.py
@@ -9,19 +9,45 @@ See https://github.com/OpenNewsLabs/open-project-linter/issues/21 .
 
 import os
 
-# TODO: consider object structure for results
 # TODO: consider errors/failure modes
 
-def check_readme(directory):
-    result = check_file_presence('README', directory)
+def check_file_presence(keyword, directory):
+    """
+    Checks whether a given directory contains a file whose name contains
+    a given string, using case insensitive matching.
+
+    Parameters
+    ----------
+    keyword : a string containing the term to search for
+    directory: a string containing the path to the directory to search in
+
+    Returns
+    -------
+    True if the keyword is found in the filenames present in
+        the directory and the corresponding file has content
+    None if the keyword is found in the filenames present in the
+        directory but the file has size 0 B
+    False if the keyword is not found in the filenames
+        present in the directory
+    """
+    files = os.listdir(directory)
+    # this is unfortunately nested, FIXME?
+    for f in files:
+        if os.path.isfile(f):
+            # in case there's anything in the path that matches keyword,
+            # only match the filename
+            filename = os.path.split(f)[1]
+            if keyword.casefold() in filename.casefold():
+                if check_for_file_content(os.path.join(directory,f)):
+                    return True
+                else:
+                    return None
+
+    # if loop finishes, file name not found
+    return False
 
 
-def check_license(directory):
-    result = check_file_presence('LICENSE', directory)
-
-def check_file_presence(filename, directory):
-    # check files in directory against given filename
-    # this will require mucking with os to read dir contents
-    # return true/false if found
-    # probably check for case-insensitive substring of file names
-    pass
+def check_for_file_content(filename):
+    """Return True if the file has > 0 B, False otherwise."""
+    # note this gives FileNotFoundError if there is no file at filepath
+    return os.path.getsize(filename) > 0
