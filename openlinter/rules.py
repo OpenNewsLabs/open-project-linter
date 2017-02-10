2e3c9f4c2a6b4ab9a90d49a3b02f32fb12abfa49
Frances Hocutt <frances.hocutt+gh@gmail.com>
2017-02-10 01:12:14 -0800

    [WIP][#13, #14, #15] Start adding version control/git rules
    * Add version control (git) and branching checks to openlinter.py
    * Add methods to rules.py to:
        * detect a git repository
        * check whether a repository has multiple branches
        * check whether some branch in the repository has a certain name
    * Stub out methods for more git interaction
        * check length of commit history
        * check for signed commits in git log

--- openlinter/rules.py
+++ openlinter/rules.py
@@ -9,6 +9,9 @@ See https://github.com/OpenNewsLabs/open-project-linter/issues/21 .
 
 import os
 
+import git
+
+
 # TODO: consider errors/failure modes
 
 def check_file_presence(keyword, directory):
@@ -51,3 +54,52 @@ def check_for_file_content(filename):
     """Return True if the file has > 0 B, False otherwise."""
     # note this gives FileNotFoundError if there is no file at filepath
     return os.path.getsize(filename) > 0
+
+
+def detect_version_control(directory):
+    """Figure out whether the directory is under version control."""
+    # possibly in the future could recurse and see if it's a subfolder?
+    version_control_system = None
+
+    if os.path.isdir(os.path.join(directory, '.git/')):
+        version_control_system = 'git'
+
+    # Could add checks for others here: CVS, svn, hg
+
+    return version_control_system
+
+
+def check_multiple_branches(repository):
+    repo = git.Repo(repository)
+    branches = repo.branches
+    if len(branches) > 1:
+        return True
+    else:
+        return False
+
+
+def check_for_develop_branch(repository, dev_branch_name):
+    repo = git.Repo(repository)
+    branches = repo.branches
+    for branch in branches:
+        if dev_branch_name in branch.name:
+            break
+    else:
+        return False
+
+    return True
+
+
+# TODO: figure out Head API, flesh this out
+def check_for_multiple_commits(repository):
+    repo = git.Repo(repository)
+    branches = Repo.branches
+    for branch in branches:
+        pass
+
+# TODO: figure out how to do this, flesh this out
+def check_for_signed_commits(repository):
+    repo = git.Repo(repository)
+    branches = repo.branches
+    for branch in branches:
+        pass
